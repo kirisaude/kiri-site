827bdecd817773b45459a14306a1f2180249c087
Iohana Goncalves Marques <iohanagmarques@MacBook-Air-de-Iohana.local>
2026-06-21 19:36:05 -0300

    OG image: símbolo + Kiri em Georgia, tagline sem espaço duplo

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -21,15 +21,18 @@ export default async function Image() {
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzNjAgMjYwIiB3aWR0aD0iMzYwIiBoZWlnaHQ9IjI2MCIgcm9sZT0iaW1nIiBhcmlhLWxhYmVsPSJLaXJpIj4KICAKICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgxMjAgNikgc2NhbGUoMSkiPgogICAgPHBhdGggZD0iTTE5IDUyIEMzMSAzNyA0MyAzNyA1MiA1MCBDNjEgMzcgNzMgMzcgODQgNTIiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0JFNkU0RSIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48L3BhdGg+CiAgICA8cGF0aCBkPSJNMzggODggQzUwIDczIDYyIDczIDcxIDg2IEM4MCA3MyA5MiA3MyAxMDQgODgiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzQ0NjA2QyIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48L3BhdGg+CiAgPC9nPgogIAogIDx0ZXh0IHg9IjE4MCIgeT0iMjM1IiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBmb250LWZhbWlseT0iTmV3c3JlYWRlciwgR2VvcmdpYSwgJiMzOTtUaW1lcyBOZXcgUm9tYW4mIzM5Oywgc2VyaWYiIGZvbnQtc2l6ZT0iMTE2IiBmb250LXdlaWdodD0iNTAwIiBmaWxsPSIjQkU2RTRFIj5LaXJpPC90ZXh0Pgo8L3N2Zz4="
-          width={360}
-          height={260}
-          alt="Kiri"
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjAgMTIwIiB3aWR0aD0iMTIwIiBoZWlnaHQ9IjEyMCIgcm9sZT0iaW1nIiBhcmlhLWxhYmVsPSJLaXJpIj4KICA8cGF0aCBkPSJNMTkgNTIgQzMxIDM3IDQzIDM3IDUyIDUwIEM2MSAzNyA3MyAzNyA4NCA1MiIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjQkU2RTRFIiBzdHJva2Utd2lkdGg9IjEwIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiPjwvcGF0aD4KICA8cGF0aCBkPSJNMzggODggQzUwIDczIDYyIDczIDcxIDg2IEM4MCA3MyA5MiA3MyAxMDQgODgiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzQ0NjA2QyIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48L3BhdGg+Cjwvc3ZnPg=="
+          width={100}
+          height={100}
+          alt=""
         />
-        <div style={{ fontSize: "26px", color: "#44606C", marginTop: "20px", fontFamily: "system-ui, sans-serif", fontWeight: "500", textAlign: "center", lineHeight: 1.4, display: "flex" }}>
+        <div style={{ fontSize: "96px", fontWeight: "500", color: "#BE6E4E", letterSpacing: "-0.02em", lineHeight: 1, fontFamily: "Georgia, serif", display: "flex", marginTop: "8px" }}>
+          Kiri
+        </div>
+        <div style={{ fontSize: "26px", color: "#44606C", marginTop: "24px", fontFamily: "Georgia, serif", fontWeight: "400", textAlign: "center", lineHeight: 1.4, display: "flex" }}>
           Rede selecionada de neurodesenvolvimento infantil
         </div>
-        <div style={{ fontSize: "18px", color: "#9A8C78", marginTop: "20px", fontFamily: "system-ui, sans-serif", display: "flex" }}>
+        <div style={{ fontSize: "18px", color: "#9A8C78", marginTop: "20px", fontFamily: "Georgia, serif", display: "flex" }}>
           kirisaude.com.br
         </div>
       </div>
