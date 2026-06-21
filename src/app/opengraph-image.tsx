dab85468bb81639707626be4fec0060102efc00f
Iohana Goncalves Marques <iohanagmarques@MacBook-Air-de-Iohana.local>
2026-06-21 19:29:54 -0300

    OG image: logo real da Kiri embutida como base64

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -17,17 +17,19 @@ export default async function Image() {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          gap: "0px",
         }}
       >
-        <div style={{ fontSize: "120px", fontWeight: "500", color: "#BE6E4E", letterSpacing: "-0.02em", lineHeight: 1, fontFamily: "Georgia, serif", display: "flex" }}>
-          Kiri
-        </div>
-        <div style={{ width: "56px", height: "3px", backgroundColor: "#BE6E4E", marginTop: "28px", borderRadius: "2px", display: "flex" }} />
-        <div style={{ fontSize: "30px", color: "#44606C", marginTop: "28px", fontFamily: "system-ui, sans-serif", fontWeight: "500", textAlign: "center", lineHeight: 1.4, display: "flex" }}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzNjAgMjYwIiB3aWR0aD0iMzYwIiBoZWlnaHQ9IjI2MCIgcm9sZT0iaW1nIiBhcmlhLWxhYmVsPSJLaXJpIj4KICAKICA8ZyB0cmFuc2Zvcm09InRyYW5zbGF0ZSgxMjAgNikgc2NhbGUoMSkiPgogICAgPHBhdGggZD0iTTE5IDUyIEMzMSAzNyA0MyAzNyA1MiA1MCBDNjEgMzcgNzMgMzcgODQgNTIiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI0JFNkU0RSIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48L3BhdGg+CiAgICA8cGF0aCBkPSJNMzggODggQzUwIDczIDYyIDczIDcxIDg2IEM4MCA3MyA5MiA3MyAxMDQgODgiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzQ0NjA2QyIgc3Ryb2tlLXdpZHRoPSIxMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48L3BhdGg+CiAgPC9nPgogIAogIDx0ZXh0IHg9IjE4MCIgeT0iMjM1IiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBmb250LWZhbWlseT0iTmV3c3JlYWRlciwgR2VvcmdpYSwgJiMzOTtUaW1lcyBOZXcgUm9tYW4mIzM5Oywgc2VyaWYiIGZvbnQtc2l6ZT0iMTE2IiBmb250LXdlaWdodD0iNTAwIiBmaWxsPSIjQkU2RTRFIj5LaXJpPC90ZXh0Pgo8L3N2Zz4="
+          width={360}
+          height={260}
+          alt="Kiri"
+        />
+        <div style={{ fontSize: "26px", color: "#44606C", marginTop: "20px", fontFamily: "system-ui, sans-serif", fontWeight: "500", textAlign: "center", lineHeight: 1.4, display: "flex" }}>
           Rede selecionada de neurodesenvolvimento infantil
         </div>
-        <div style={{ fontSize: "20px", color: "#9A8C78", marginTop: "32px", fontFamily: "system-ui, sans-serif", display: "flex" }}>
+        <div style={{ fontSize: "18px", color: "#9A8C78", marginTop: "20px", fontFamily: "system-ui, sans-serif", display: "flex" }}>
           kirisaude.com.br
         </div>
       </div>
